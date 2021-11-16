39f6b475c3b04c4e00261e9baa8f4b7fb0e07562
mopeneko <mopeneko@lem0n.cc>
2021-11-17 05:41:33 +0900

    Chakra UIのLinkスタイルが適用されるようにした

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,11 +4,13 @@ import { Flex, Link, Text } from '@chakra-ui/layout';
 const Header = () => {
   return (
     <Flex as="header" p={6} bg="teal.300" color="white">
-      <Link as={NextLink} href="/">
-        <Text as="h1" fontSize="3xl">
-          もぺブログ
-        </Text>
-      </Link>
+      <NextLink href="/">
+        <Link>
+          <Text as="h1" fontSize="3xl">
+            もぺブログ
+          </Text>
+        </Link>
+      </NextLink>
     </Flex>
   );
 };
