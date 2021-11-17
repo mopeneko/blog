52394d2cf9728bfb2bcfafafcb5f8a1ade49f031
RimlTempest <riml.slime@gmail.com>
2021-11-17 10:26:03 +0900

    update: Seoのコンポーネント化

---
+++ components/wrapper/Seo.tsx
@@ -0,0 +1,53 @@
+import { NextSeo } from 'next-seo'
+
+type SeoProps = {
+  title: string
+  url: string
+  description: string
+}
+
+const SEO = (props: SeoProps) => {
+  const twitterId = '@_m_vt'
+  return (
+    <NextSeo
+      title={props.title}
+      description={props.description}
+      canonical={props.url}
+      openGraph={{
+        url: props.url,
+        title: props.title,
+        description: props.description,
+        // TODO: maskableなアイコンを用意する
+        images: [
+          {
+            url: `${props.url}/maskable_icon_x384.png`,
+            width: 384,
+            height: 384,
+            alt: props.title,
+            type: 'image/png',
+          },
+          {
+            url: `${props.url}/maskable_icon_x512.png`,
+            width: 512,
+            height: 512,
+            alt: props.title,
+            type: 'image/png',
+          },
+          { url: `${props.url}/maskable_icon_x512.png` },
+        ],
+        site_name: props.title,
+      }}
+      twitter={{
+        handle: twitterId,
+        site: twitterId,
+        cardType: 'summary_large_image',
+      }}
+      // TODO: facebook対応する場合appIdの取得が必要なので検討
+      //   facebook={{
+      //     appId: '',
+      //   }}
+    />
+  )
+}
+
+export default SEO
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,35 @@
-import { GetStaticProps } from 'next';
-import { Container } from '@chakra-ui/react';
-import { NextSeo } from 'next-seo';
-import { client } from '../libs/client';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import ArticleList from '../components/ArticleList';
-import { Article } from '../types/article';
+import { GetStaticProps } from 'next'
+import { Container } from '@chakra-ui/react'
+import { NextSeo } from 'next-seo'
+import { client } from '../libs/client'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import ArticleList from '../components/ArticleList'
+import { Article } from '../types/article'
+import Seo from '../components/wrapper/Seo'
 
 type Props = {
-  articles: Article[];
-};
+  articles: Article[]
+}
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-  const data = await client.getList<Article>({ endpoint: 'articles' });
+  const data = await client.getList<Article>({ endpoint: 'articles' })
 
   return {
     props: {
       articles: data.contents,
     },
-  };
-};
+  }
+}
 
 const Home: React.FC<Props> = ({ articles }) => {
   return (
     <>
-      <NextSeo title="もぺブログ" />
+      <Seo
+        title="もぺブログ"
+        url="https://blog.lem0n.cc"
+        description="もぺねこのぶろぐ！"
+      />
 
       <Header />
       <Container maxW="container.md">
@@ -32,7 +37,7 @@ const Home: React.FC<Props> = ({ articles }) => {
       </Container>
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
