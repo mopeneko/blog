c0c9def63ebcc74a4772d75bea24f6407c6c3df1
huequica <dev@huequica.xyz>
2021-11-20 12:45:05 +0900

    rename and move type in module definition

---
+++ types/article.d.ts
@@ -0,0 +1,11 @@
+declare module 'mopeneko_blog' {
+  export type Article = {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    revisedAt: string;
+    title: string;
+    content: string;
+  };
+}
