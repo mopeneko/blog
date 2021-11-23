65c63ef42526762bc26a00bf6ad1a1682d98a4ad
mopeneko <mopeneko@lem0n.cc>
2021-11-24 03:14:37 +0900

    Fix lint

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import { Flex, Text } from '@chakra-ui/layout';
 
 const Footer = () => {
   return (
-    <Flex as="footer" p={6} bg="gray.100" color="black" justifyContent="center">
+    <Flex as='footer' p={6} bg='gray.100' color='black' justifyContent='center'>
       <Text>mopeneko</Text>
     </Flex>
   );
