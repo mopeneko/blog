d17fd697ec1def71ec671f2166b99b1c4b882362
mopeneko <mopeneko@lem0n.cc>
2021-11-30 15:12:42 +0900

    フォントを設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import { theme } from 'theme';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
+import '@fontsource/open-sans';
+import '@fontsource/noto-sans-jp';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
---
+++ theme.ts
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  fonts: {
+    body: 'Noto Sans JP, Open Sans, sans-serif',
+    heading: 'Noto Sans JP, Open Sans, serif',
+    mono: 'JetBrains Mono, monospace',
+  }
+})
